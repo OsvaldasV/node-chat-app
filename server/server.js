183c6c5a82b49a2c502c1604f7fcddcbc88ae886
OsvaldasV <Osvaldas.Vaivada@gmail.com>
2017-02-24 10:31:00 +0200

    Added new message and create message events

--- server/server.js
+++ server/server.js
@@ -15,6 +15,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New user connected');
 
+	socket.emit('newMessage', {
+		from: 'VasiaSuper@myliukas.lt',
+		text: 'Valio!',
+		createdAt: 123
+	});
+
+	socket.on('createMessage', (newMessage) => {
+		console.log('createMessage', newMessage);
+	});
+
 	socket.on('disconnect', () => {
 		console.log('User disconnected from the server');
 	});
