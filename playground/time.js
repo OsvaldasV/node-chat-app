3ac53f369f415eaaba4e098b49b869ae6c7a3db3
OsvaldasV <Osvaldas.Vaivada@gmail.com>
2017-02-28 10:10:54 +0200

    Formatted timestamps using moment js

---
+++ playground/time.js
@@ -0,0 +1,8 @@
+var moment = require('moment');
+
+
+var date = moment();
+// moments.js dokumentacijoj yra optionai
+// console.log(date.format('MMM Do, YYYY'));
+
+// console.log(date.format('h:mm a'))
